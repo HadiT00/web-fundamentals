323a69d2a09325d0889d3e4e76f27cfbfdfc8273
HadiT00 <91471503+HadiT00@users.noreply.github.com>
2021-12-28 23:50:56 +0100

    Add files via upload

---
+++ Sorting Hat/Add2House.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="assests/css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="Add">
+        <form action="/index.php">
+            <div id="MaleFemale">
+            </div>
+            <div class="AllHouses">
+                var counter = 0;
+                <div id="HouseGryffindor">
+                    <button type="button">Gryffindor</button>
+
+                </div>
+                <div id="HouseRavenclaw">
+                    <button type="button">Ravenclaw</button>
+                </div>
+                <div id="HouseSlytherin">
+                    <button type="button">Slytherin</button>
+                </div>
+                <div id="HouseHufflepuff">
+                    <button type="button">Hufflepuff</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
